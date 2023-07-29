7722912c52662a67842a3b5b9c3e6c7217e52975
jon <jon6566@gmail.com>
2023-07-29 10:19:36 -0700

    .

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy import text
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 
 
 app = Flask(__name__)
@@ -27,8 +27,9 @@ def list_users():
     return render_template("list.html", users=users)
 
 
-@app.route("/", methods=["POST"])
+@app.route("/", methods=["POST"], endpoint="create_user")
 def create():
+    """Handle form submission for creating a new user"""
     first_name = request.form["first_name"]
     last_name = request.form["last_name"]
     image = request.form["image"]
@@ -36,6 +37,7 @@ def create():
     new_user = User(first_name=first_name, last_name=last_name, image=image)
     db.session.add(new_user)
     db.session.commit()
+    flash(f"User {new_user.first_name} added.")
 
     return redirect(f"/{new_user.id}")
 
@@ -44,11 +46,12 @@ def create():
 def show_user(user_id):
     """Show details about a single user"""
     user = User.query.get_or_404(user_id)
-    return render_template("details.html", user=user)
+    posts = Post.query.all()
+    return render_template("details.html", user=user, posts=posts)
 
 
 @app.route("/create_user")
-def create_user():
+def show_create_user_form():
     "Shows form to create new user"
     return render_template("form.html")
 
@@ -64,7 +67,7 @@ def delete_user(user_id):
 
 @app.route("/edit/<int:user_id>", methods=["GET"])
 def edit_user(user_id):
-    "Shows form to create new user"
+    "Shows form to edit user"
     user = User.query.get_or_404(user_id)
     return render_template("edit.html", user=user)
 
@@ -90,3 +93,67 @@ def edit(user_id):
     db.session.commit()
 
     return redirect("/")
+
+
+@app.route("/add_post/<int:user_id>")
+def show_create_post_form(user_id):
+    """Shows form to create a new post"""
+    user = User.query.get_or_404(user_id)
+    return render_template("post_form.html", user=user)
+
+
+@app.route("/new_post/<int:user_id>", methods=["POST"], endpoint="create_post")
+def create_post(user_id):
+    """Handles form submission to create a new post"""
+    title = request.form["title"]
+    content = request.form["content"]
+
+    user = User.query.get_or_404(user_id)
+    new_post = Post(title=title, content=content, user=user)
+    db.session.add(new_post)
+    db.session.commit()
+
+    return redirect(f"/{user_id}")
+
+
+@app.route("/post/details/<int:post_id>")
+def post_details(post_id):
+    """show details about post"""
+    post = Post.query.get_or_404(post_id)
+    return render_template("post_details.html", post=post)
+
+
+@app.route("/post/delete/<int:post_id>", methods=["POST"])
+def delete_post(post_id):
+    "Deletes post"
+    post = Post.query.get_or_404(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect("/")
+
+
+@app.route("/post/edit/<int:post_id>", methods=["GET"])
+def edit_post(post_id):
+    "Shows form to edit post"
+    post = Post.query.get_or_404(post_id)
+    return render_template("edit_post.html", post=post)
+
+
+@app.route("/post/edit/<int:post_id>", methods=["POST"])
+def post_edit(post_id):
+    post = Post.query.get_or_404(post_id)
+    new_title = request.form["title"]
+    if not new_title:
+        new_title = post.title
+    new_content = request.form["content"]
+    if not new_content:
+        new_content = post.content
+
+    post.title = new_title
+    post.content = new_content
+    post.user_id = post.user_id
+
+    db.session.add(post)
+    db.session.commit()
+
+    return redirect(f"/post/details/{post.id}")
--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 """Models for Blogly."""
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, DateTime, func
 
 # from sqlalchemy import text
 
@@ -12,7 +13,7 @@ def connect_db(app):
 
 
 class User(db.Model):
-    __tanblename__ = "users"
+    __tablename__ = "users"
 
     def __repr__(self):
         u = self
@@ -27,5 +28,23 @@ class User(db.Model):
         default=" https://media.istockphoto.com/id/1143500885/vector/man-portrait-with-beard-vector-illustration-of-male-character.jpg?s=612x612&w=0&k=20&c=v9pv64ASjUYTRscfrYXEeof-oI2IfTPwJPuxRWNeG74=",
     )
 
+    posts = db.relationship("Post", backref="user", cascade="all, delete-orphan")
+
     def greet(self):
         return f" Hi, I'm {self.first_name} {self.last_name} and this is my picture"
+
+
+class Post(db.Model):
+    __tablename__ = "posts"
+
+    def __repr__(self):
+        p = self
+        return f" Post id={p.id} title={p.title} content{p.content} created_at={p.created_at}"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(30), nullable=False, unique=True)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    # us = db.relationship("User", backref="posts")
