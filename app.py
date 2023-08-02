c7b56ffc70b78b80dcef09e48f96106af350a860
jon <jon6566@gmail.com>
2023-08-02 04:27:32 -0700

    .

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy import text
-from models import db, connect_db, User, Post
+from models import db, connect_db, User, Post, Tag, PostTag
 
 
 app = Flask(__name__)
@@ -46,7 +46,7 @@ def create():
 def show_user(user_id):
     """Show details about a single user"""
     user = User.query.get_or_404(user_id)
-    posts = Post.query.all()
+    posts = Post.query.filter(Post.user_id == user_id).all()
     return render_template("details.html", user=user, posts=posts)
 
 
@@ -99,7 +99,8 @@ def edit(user_id):
 def show_create_post_form(user_id):
     """Shows form to create a new post"""
     user = User.query.get_or_404(user_id)
-    return render_template("post_form.html", user=user)
+    tags = Tag.query.all()
+    return render_template("post_form.html", user=user, tags=tags)
 
 
 @app.route("/new_post/<int:user_id>", methods=["POST"], endpoint="create_post")
@@ -109,7 +110,11 @@ def create_post(user_id):
     content = request.form["content"]
 
     user = User.query.get_or_404(user_id)
-    new_post = Post(title=title, content=content, user=user)
+    tag_ids = [int(num) for num in request.form.getlist("tags")]
+    tags = Tag.query.filter(Tag.id.in_(tag_ids)).all()
+
+    new_post = Post(title=title, content=content, user=user, tags=tags)
+
     db.session.add(new_post)
     db.session.commit()
 
@@ -136,7 +141,8 @@ def delete_post(post_id):
 def edit_post(post_id):
     "Shows form to edit post"
     post = Post.query.get_or_404(post_id)
-    return render_template("edit_post.html", post=post)
+    tags = Tag.query.all()
+    return render_template("edit_post.html", post=post, tags=tags)
 
 
 @app.route("/post/edit/<int:post_id>", methods=["POST"])
@@ -153,7 +159,76 @@ def post_edit(post_id):
     post.content = new_content
     post.user_id = post.user_id
 
+    tag_ids = [int(num) for num in request.form.getlist("tags")]
+    post.tags = Tag.query.filter(Tag.id.in_(tag_ids)).all()
+
     db.session.add(post)
     db.session.commit()
+    flash(f"Post '{post.title}' edited.")
 
     return redirect(f"/post/details/{post.id}")
+
+
+@app.route("/GET/tags")
+def list_tags():
+    """Shows a list of all tags in db"""
+    tags = Tag.query.all()
+    return render_template("tag_list.html", tags=tags)
+
+
+@app.route("/GET/tags/new", methods=["GET"])
+def new_tag_form():
+    """Shows form to create a new tag"""
+    return render_template("tag_form.html")
+
+
+@app.route("/POST/tags/new", methods=["POST"])
+def create_tag():
+    """handle form submission for creating a new tag"""
+    name = request.form["name"]
+
+    new_tag = Tag(name=name)
+    db.session.add(new_tag)
+    db.session.commit()
+    flash("New TAG has been created")
+
+    return redirect("/GET/tags")
+
+
+@app.route("/GET/tags/<int:tag_id>")
+def tag_details(tag_id):
+    """ "Shows details about the tag"""
+    tag = Tag.query.get_or_404(tag_id)
+
+    return render_template("tag_details.html", tag=tag)
+
+
+@app.route("/POST/tags/<int:tag_id>/delete")
+def delete_tag(tag_id):
+    tag = Tag.query.get_or_404(tag_id)
+    db.session.delete(tag)
+    db.session.commit()
+    return redirect("/GET/tags")
+
+
+@app.route("/GET/tags/<int:tag_id>/edit")
+def edit_tag(tag_id):
+    """Shows form to edit tag"""
+    tag = Tag.query.get_or_404(tag_id)
+    return render_template("edit_tag.html", tag=tag)
+
+
+@app.route("/POST/tags/<int:tag_id>/edit", methods=["POST"])
+def edit_tag_post(tag_id):
+    """Makes the edition to the tag"""
+    tag = Tag.query.get_or_404(tag_id)
+    new_name = request.form["name"]
+    if not new_name:
+        new_name = tag.name
+
+    tag.name = new_name
+
+    db.session.add(tag)
+    db.session.commit()
+
+    return redirect("/GET/tags")
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 """Models for Blogly."""
+import datetime
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, DateTime, func
 
@@ -44,7 +45,34 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(30), nullable=False, unique=True)
     content = db.Column(db.Text, nullable=False)
-    created_at = db.Column(db.DateTime, default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
 
     # us = db.relationship("User", backref="posts")
+    tags = db.relationship(
+        "Tag",
+        secondary="post_tag",
+        backref="associated_post",
+        cascade="all",
+        single_parent=True,
+    )
+
+    @property
+    def friendly_date(self):
+        """Return nicely-formatted date."""
+
+        return self.created_at.strftime("%a %b %-d  %Y, %-I:%M %p")
+
+
+class Tag(db.Model):
+    __tablename__ = "tags"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, nullable=False, unique=True)
+
+
+class PostTag(db.Model):
+    __tablename__ = "post_tag"
+
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey("tags.id"), primary_key=True)
